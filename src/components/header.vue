20f979cee0349cc63d7335d567da84452dec6d6d
thanh <hoangthanh@gxd.vn>
2021-04-03 22:53:00 +0700

    toi 03/04

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,12 +1,11 @@
 <template>
-<div>
+<div class="comp-header">
   <b-button-toolbar aria-label="Toolbar with button groups and dropdown menu">
-  <b-dropdown  right text="Tiện Ích">
+  <b-dropdown class="ultilyti" right text="Tiện Ích">
       <b-dropdown-item>Item 1</b-dropdown-item>
       <b-dropdown-item>Item 2</b-dropdown-item>
       <b-dropdown-item>Item 3</b-dropdown-item>
     </b-dropdown>
-      <div class="col-6">
         <div class="input-group">
             <input 
                 v-bind:value="strSearch"
@@ -18,7 +17,6 @@
                     class="btn btn-info" type="button">Clear!</button>
             </span>
         </div>
-    </div>
     <b-dropdown class="acount" right text="Nguyễn Hồng Quang">
       <b-dropdown-item>Item 1</b-dropdown-item>
       <b-dropdown-item>Item 2</b-dropdown-item>
@@ -41,10 +39,32 @@ export default {
 </script>
 
 <style>
-.acount> .btn-secondary {
-   margin-left: 360px;
+
+.acount >.btn-secondary{
+  background-color: #fff;
+  color: #000;
+  border: none;
+  margin-right: 5px;
+}
+.ultilyti >.btn-secondary{
+  background-color: #fff;
+  color: #000;
+  border: none;
+  margin-left: 5px;
+}
+
+.acount{
+  position: absolute;
+  right: 0;
 }
 .input-group{
-  margin-left: 300px;
+}
+.btn-toolbar {
+ position: relative;
+}
+.btn-toolbar >.input-group {
+    width: 40%;
+    right: 30%;
+    position:absolute;
 }
 </style>
--- src/components/navigation.vue
+++ src/components/navigation.vue
@@ -3,55 +3,55 @@
  <div class="nav">
   <b-button-toolbar aria-label="Toolbar with button groups and dropdown menu">
    
-    <b-dropdown class="mx-1" right text="Trang chủ">
+    <b-dropdown class="mx-1 page-home" right text="Trang chủ">
       <b-dropdown-item>Item 1</b-dropdown-item>
       <b-dropdown-item>Item 2</b-dropdown-item>
       <b-dropdown-item>Item 3</b-dropdown-item>
     </b-dropdown>
 
-    <b-dropdown class="mx-1" right text="Công việc">
+    <b-dropdown class="mx-1 nav-tvgs" right text="Công việc">
       <b-dropdown-item>Item 1</b-dropdown-item>
       <b-dropdown-item>Item 2</b-dropdown-item>
       <b-dropdown-item>Item 3</b-dropdown-item>
     </b-dropdown>
 
-    <b-dropdown class="mx-1" right text="Báo cáo">
+    <b-dropdown class="mx-1 nav-tvgs" right text="Báo cáo">
       <b-dropdown-item>Item 1</b-dropdown-item>
       <b-dropdown-item>Item 2</b-dropdown-item>
       <b-dropdown-item>Item 3</b-dropdown-item>
     </b-dropdown>
     
-    <b-dropdown class="mx-1" right text="Văn bản & tài liệu">
+    <b-dropdown class="mx-1 nav-tvgs" right text="Văn bản & tài liệu">
       <b-dropdown-item>Item 1</b-dropdown-item>
       <b-dropdown-item>Item 2</b-dropdown-item>
       <b-dropdown-item>Item 3</b-dropdown-item>
     </b-dropdown>
     
-    <b-dropdown class="mx-1" right text="Hồ sơ">
+    <b-dropdown class="mx-1 nav-tvgs" right text="Hồ sơ">
       <b-dropdown-item>Item 1</b-dropdown-item>
       <b-dropdown-item>Item 2</b-dropdown-item>
       <b-dropdown-item>Item 3</b-dropdown-item>
     </b-dropdown>
     
-    <b-dropdown class="mx-1" right text="Hợp đồng">
+    <b-dropdown class="mx-1 nav-tvgs" right text="Hợp đồng">
       <b-dropdown-item>Item 1</b-dropdown-item>
       <b-dropdown-item>Item 2</b-dropdown-item>
       <b-dropdown-item>Item 3</b-dropdown-item>
     </b-dropdown>
     
-    <b-dropdown class="mx-1" right text="Nhân sự">
+    <b-dropdown class="mx-1 nav-tvgs" right text="Nhân sự">
       <b-dropdown-item>Item 1</b-dropdown-item>
       <b-dropdown-item>Item 2</b-dropdown-item>
       <b-dropdown-item>Item 3</b-dropdown-item>
     </b-dropdown>
     
-    <b-dropdown class="mx-1" right text="Hệ thống">
+    <b-dropdown class="mx-1 nav-tvgs" right text="Hệ thống">
       <b-dropdown-item>Item 1</b-dropdown-item>
       <b-dropdown-item>Item 2</b-dropdown-item>
       <b-dropdown-item>Item 3</b-dropdown-item>
     </b-dropdown>
     
-    <b-dropdown class="mx-1" right text="Dự án">
+    <b-dropdown class="mx-1 nav-tvgs" right text="Dự án">
       <b-dropdown-item>Item 1</b-dropdown-item>
       <b-dropdown-item>Item 2</b-dropdown-item>
       <b-dropdown-item>Item 3</b-dropdown-item>
@@ -73,9 +73,16 @@ name:'navi-tvgs'
     background-color:indigo;
     margin-top: 10px;
 }
-.abc > .btn-secondary {
-    color: #fff;
-    background-color: #fff;
+.nav-tvgs > .btn-secondary {
+    background-color: indigo;
+  color: #fff;
+  border: none;
+  margin-left: 20px;
 }
-
+.page-home > .btn-secondary {
+    background-color: indigo;
+  color: #fff;
+  border: none;
+  margin-left: 5px;
+}
 </style>
