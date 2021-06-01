9739ebed0f0db932588f81dc4375292973e7a6cb
HT <hoangthanh@gxd.vn>
2021-06-01 03:30:01 -0700

    endday

--- apiGsxd/resources/js/app.js
+++ apiGsxd/resources/js/app.js
@@ -1,1 +1,8 @@
 require('./bootstrap');
+import Vue from 'vue'
+import App from './vue/app.vue'
+window.Vue = require('vue').default;
+const app = new Vue({
+    el:'#app',
+    components: {App}
+});
--- tvgsweb/src/plugins/axios.js
+++ tvgsweb/src/plugins/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const axiosInstance = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api/',
+    baseURL: 'http://vue.gxd.vn/api/',
     headers: {'Content-Type': 'application/json'}
   });
 
